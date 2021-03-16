4944b0a0d6bcddd5cd4bafc829602f1e2897007c
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-17 00:21:13 +0530

    Update Arrays.c

--- Arrays.c
+++ Arrays.c
@@ -1,6 +1,38 @@
 //Programs using Arrays:
 
-// 1) Taking Input for 2 Arrays a[] and b[] and adding them both, where user can give the num --> no. of values to input in arrays.
+// 1) Basic Array ( takes Input and prints out what the user has entered!!)
+
+#include<stdio.h>
+int main()
+{
+ int a[50][50], num;
+ printf("give the no.of inputs the arrays has to take (No more than 50) :");  scanf("%i",&num);
+ /* program for taking in values in array a[] and printing them out:*/
+ for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("Enter the value in a[%i][%i] : ",i,j);  scanf("%i",&a[i][j]);
+    }
+  printf("\n");
+ }
+ 
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("%i\t",a[i][j]);
+    }
+  printf("\n");
+ }
+ getch();
+ return 0;
+} 
+
+//_________________________________________________________________________________________________________________________________________________________________
+
+
+// 2) Taking Input for 2 Arrays a[] and b[] and adding them both, where user can give the num --> no. of values to input in arrays.
 
 #include<stdio.h>
 int main()
@@ -65,7 +97,7 @@ int main()
 
 //_________________________________________________________________________________________________________________________________________________________________
 
-// 2) Write a Program to Transverse a array:
+// 3) Write a Program to Transverse a array:
 
 
 #include<stdio.h>
