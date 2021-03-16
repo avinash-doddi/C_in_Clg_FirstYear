4aba7e69ca6748df998a3964cc7c3bbcad52be62
Doddi Avinash <76215983+avinash-doddi@users.noreply.github.com>
2021-03-16 19:43:15 +0530

    Add files via upload

---
+++ Arrays.c
@@ -0,0 +1,109 @@
+//Programs using Arrays:
+
+// 1) Taking Input for 2 Arrays a[] and b[] and adding them both, where user can give the num --> no. of values to input in arrays.
+
+#include<stdio.h>
+int main()
+{
+ int a[50][50] , b[50][50], num;
+ printf("give the no.of inputs the arrays has to take (No more than 50) :");  scanf("%i",&num);
+ /* program for taking in values in array a[] and printing them out:*/
+ for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("Enter the value in a[%i][%i] : ",i,j);  scanf("%i",&a[i][j]);
+    }
+  printf("\n");
+ }
+ 
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("%i\t",a[i][j]);
+    }
+  printf("\n");
+ }
+ 
+ /*program for taking and printing values in array b[]*/
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("Enter the value in b[%i][%i] : ",i,j);  scanf("%i",&b[i][j]);
+    }
+  printf("\n");
+ }
+ 
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("%i\t",b[i][j]);
+    }
+  printf("\n");
+ }
+ 
+ /*program for adding array a[] & b[] and printing them out */
+ 
+ printf("\n");    printf("The sum of array a[] and b[] is :\n");
+ 
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+       
+    printf("%i\t",a[i][j]+b[i][j]);
+    }
+  printf("\n");
+ }
+ 
+ getch(); //getch() is not required for Visual studio Code and other online Compilers..
+ return 0;
+}
+
+//_____________________________________________________________________________________________________________________________________
+
+// 2) Write a Program to Transverse a array:
+
+
+#include<stdio.h>
+int main()
+{
+int a[50][50], num;
+printf("give the no.of inputs the arrays has to take (No more than 50) :"); scanf("%i",&num);
+ 
+ /* program for taking in values in array a[] and printing them out:*/
+ for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("Enter the value in a[%i][%i] : ",i,j);  scanf("%i",&a[i][j]);
+    }
+  printf("\n");
+ }
+ 
+  for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("%i\t",a[i][j]);
+    }
+  printf("\n");
+ }
+ 
+printf("\n");  printf("The transverse of a[][] is : \n");
+   for (int i = 0; i<num ; i++)
+ {
+   for (int j = 0; j<num ; j++)
+    {
+    printf("%i\t",a[j][i]);
+    }
+  printf("\n");
+ }
+getch();
+return 0;
+}
+
+
